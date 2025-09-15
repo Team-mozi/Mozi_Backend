9a71744b7a169e760ab25c6544bbd4d095641746
pia01190 <piakim0119@gmail.com>
2025-09-16 08:57:57 +0900

    feature: BaseEntity 생성

---
+++ src/main/java/com/mozi/global/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.mozi.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    protected Boolean activated = true;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    public void unActivated() {
+        this.activated = false;
+    }
+}
