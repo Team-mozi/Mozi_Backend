785be6322f3d1ce927ea25c41009d0476d0378d9
dpwl0974 <dpwl0974@gmail.com>
2025-09-15 23:35:39 +0900

    feat: data 없는 성공 응답 메서드 추가

--- src/main/java/com/mozi/global/common/ApiResponse.java
+++ src/main/java/com/mozi/global/common/ApiResponse.java
@@ -21,6 +21,11 @@ public class ApiResponse <T> {
         return new ApiResponse<>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMsg(), data);
     }
 
+    // 성공 응답 - data 없음
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMsg(), null);
+    }
+
     // 요청 실패 응답
     public static <T> ApiResponse<T> error(ResultCode resultCode) {
         return new ApiResponse<>(resultCode.getCode(), resultCode.getMsg(), null);
