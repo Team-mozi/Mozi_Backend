efef260c93f16410b83c72270d04947aa79c1eb4
pia01190 <piakim0119@gmail.com>
2025-09-16 18:03:00 +0900

    feature: Image 엔티티 작성 및 dto에 image 추가

--- src/main/java/com/mozi/domain/emoji/controller/dto/request/UserEmojiCreateRequest.java
+++ src/main/java/com/mozi/domain/emoji/controller/dto/request/UserEmojiCreateRequest.java
@@ -1,16 +1,21 @@
 package com.mozi.domain.emoji.controller.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class UserEmojiCreateRequest {
 
-    @NotBlank
+    @NotNull
     @Schema(description = "이모지 번호", example = "1")
     private Long emojiId;
 
-    @Schema(description = "내용", example = "공부를 했다.")
+    @Schema(description = "내용", example = "공부하는중!")
     private String text;
+
+    @Schema(description = "이미지 URL 목록", example = "http://localhost:8080/img1.jpg")
+    private List<String> imageUrls;
 }
