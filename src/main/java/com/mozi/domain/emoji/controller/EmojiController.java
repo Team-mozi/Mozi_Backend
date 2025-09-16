a601c70e3f156da67c2507478bd251ddfa261180
pia01190 <piakim0119@gmail.com>
2025-09-16 16:39:48 +0900

    feature: Emoji 스웨거 작성

---
+++ src/main/java/com/mozi/domain/emoji/controller/EmojiController.java
@@ -0,0 +1,27 @@
+package com.mozi.domain.emoji.controller;
+
+import com.mozi.domain.emoji.controller.dto.response.EmojiResponse;
+import com.mozi.domain.emoji.controller.dto.response.RepresentativeEmojiResponse;
+import com.mozi.global.response.ApiResponse;
+import com.mozi.global.response.ResponseCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/api/emojis")
+@RestController
+public class EmojiController implements EmojiSpecification {
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<EmojiResponse>>> getEmojis() {
+        return ResponseEntity.ok(ApiResponse.success(ResponseCode.OK, List.of(new EmojiResponse())));
+    }
+
+    @GetMapping("/representatives")
+    public ResponseEntity<ApiResponse<List<RepresentativeEmojiResponse>>> getRepresentativeEmojis() {
+        return ResponseEntity.ok(ApiResponse.success(ResponseCode.OK, List.of(new RepresentativeEmojiResponse())));
+    }
+}
---
+++ src/main/java/com/mozi/domain/emoji/controller/dto/response/RepresentativeEmojiResponse.java
@@ -0,0 +1,17 @@
+package com.mozi.domain.emoji.controller.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class RepresentativeEmojiResponse {
+
+    @Schema(description = "이모지 번호", example = "1")
+    private Long emojiId;
+
+    @Schema(description = "이모지 url", example = "http://localhost:8080/image.jpg")
+    private String emojiUrl;
+
+    @Schema(description = "대표 여부", example = "true")
+    private boolean representative;
+}
