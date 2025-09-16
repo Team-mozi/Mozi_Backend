a143eaee4c6f8f69fa191a8e88ba4291e25f13d7
pia01190 <piakim0119@gmail.com>
2025-09-16 18:22:12 +0900

    refactor: 공통응답객체 수정

--- src/main/java/com/mozi/domain/emoji/controller/UserEmojiController.java
+++ src/main/java/com/mozi/domain/emoji/controller/UserEmojiController.java
@@ -5,7 +5,6 @@ import com.mozi.domain.emoji.controller.dto.response.RandomUserEmojiResponse;
 import com.mozi.domain.emoji.controller.dto.response.UserEmojiDetailResponse;
 import com.mozi.domain.emoji.controller.dto.response.UserEmojiResponse;
 import com.mozi.global.response.ApiResponse;
-import com.mozi.global.response.ResponseCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,26 +16,26 @@ public class UserEmojiController implements UserEmojiSpecification {
 
     @GetMapping("/latest")
     public ResponseEntity<ApiResponse<UserEmojiResponse>> getLatestUserEmoji() {
-        return ResponseEntity.ok(ApiResponse.success(ResponseCode.OK, new UserEmojiResponse()));
+        return ResponseEntity.ok(ApiResponse.success(new UserEmojiResponse()));
     }
 
     @GetMapping("/{userEmojiId}")
     public ResponseEntity<ApiResponse<UserEmojiDetailResponse>> getUserEmojiDetail(@PathVariable("userEmojiId") Long userEmojiId) {
-        return ResponseEntity.ok(ApiResponse.success(ResponseCode.OK, new UserEmojiDetailResponse()));
+        return ResponseEntity.ok(ApiResponse.success(new UserEmojiDetailResponse()));
     }
 
     @GetMapping("/random")
     public ResponseEntity<ApiResponse<List<RandomUserEmojiResponse>>> getRandomUserEmojis() {
-        return ResponseEntity.ok(ApiResponse.success(ResponseCode.OK, List.of(new RandomUserEmojiResponse())));
+        return ResponseEntity.ok(ApiResponse.success(List.of(new RandomUserEmojiResponse())));
     }
 
     @PostMapping
     public ResponseEntity<ApiResponse<Long>> createUserEmoji(@RequestBody UserEmojiCreateRequest request) {
-        return ResponseEntity.ok(ApiResponse.success(ResponseCode.OK));
+        return ResponseEntity.ok(ApiResponse.success());
     }
 
     @DeleteMapping("/{userEmojiId}")
     public ResponseEntity<ApiResponse<Void>> deleteUserEmoji(@PathVariable("userEmojiId") Long userEmojiId) {
-        return ResponseEntity.ok(ApiResponse.success(ResponseCode.OK));
+        return ResponseEntity.ok(ApiResponse.success());
     }
 }
