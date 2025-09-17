0229093a53326aa768b0b4bc16abe965fee56e09
dpwl0974 <dpwl0974@gmail.com>
2025-09-17 09:17:42 +0900

    refactor: 엔터티 재구조화

---
+++ src/main/java/com/mozi/domain/user/entity/User.java
@@ -0,0 +1,43 @@
+package com.mozi.domain.user.entity;
+
+import com.mozi.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String oauthId;
+
+    private String oauthProvider;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(unique = true)
+    private String nickname;
+
+    @Column(name = "is_agreed")
+    private boolean agreed;
+
+    protected User() {}
+
+    @Builder
+    private  User(String oauthId, String oauthProvider, String email, String password, String nickname) {
+        this.oauthId = oauthId;
+        this.oauthProvider = oauthProvider;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.agreed = true;
+    }
+
+}
