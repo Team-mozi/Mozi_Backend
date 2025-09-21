6a9fb88f11692338facacc836a4a63e89d0aca5c
dpwl0974 <dpwl0974@gmail.com>
2025-09-21 21:49:09 +0900

    feature: 회원가입 비밀번호 유효성 검사 규칙 추가

--- src/main/java/com/mozi/domain/user/controller/dto/request/RegisterRequest.java
+++ src/main/java/com/mozi/domain/user/controller/dto/request/RegisterRequest.java
@@ -3,6 +3,7 @@ package com.mozi.domain.user.controller.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
@@ -12,7 +13,9 @@ public class RegisterRequest {
     @NotBlank
     private String email;
 
-    @Schema(description = "비밀번호", example = "1234")
+    @Schema(description = "비밀번호", example = "Password123!")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,}$",
+            message = "비밀번호는 숫자, 영문, 특수문자를 혼합하여 8자 이상이어야 합니다.")
     @NotBlank
     private String password;
 
