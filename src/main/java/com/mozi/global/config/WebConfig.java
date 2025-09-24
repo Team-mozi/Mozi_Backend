c1dc79ede905c58fb5be0345b9e3ddb43439a3ea
pia01190 <piakim0119@gmail.com>
2025-09-24 12:40:13 +0900

    refactor: cors 설정 프론트 url 추가

--- src/main/java/com/mozi/global/config/WebConfig.java
+++ src/main/java/com/mozi/global/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("https://mozi.o-r.kr")
+                    .allowedOrigins("https://mozi.o-r.kr", "http://localhost:3000")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true)
