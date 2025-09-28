5779602d21d260d3c0f973d15c2655308500de32
pia01190 <piakim0119@gmail.com>
2025-09-28 11:39:04 +0900

    chore: authorize header parameter에 명시적으로 추가

--- src/main/java/com/mozi/global/config/SwaggerConfig.java
+++ src/main/java/com/mozi/global/config/SwaggerConfig.java
@@ -3,10 +3,15 @@ package com.mozi.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -30,5 +35,35 @@ public class SwaggerConfig {
                                 .bearerFormat("JWT")
                         )
                     );
+    }
+
+    @Bean
+    public GlobalOpenApiCustomizer authorizationHeaderCustomizer() {
+        return openApi -> {
+            Parameter authHeader = new Parameter()
+                .name("Authorization")
+                .description("JWT Access Token")
+                .in("header")
+                .required(false)
+                .schema(new StringSchema().example("Bearer <your_token>"));
+
+            List<String> excludedPaths = List.of(
+                "/api/users/register",
+                "/api/users/login",
+                "/api/users/email-verifications/",
+                "/api/users/reissue",
+                "/api/emojis/highlights"
+            );
+
+            openApi.getPaths().forEach((path, pathItem) -> {
+                boolean excluded = excludedPaths.stream().anyMatch(path::startsWith);
+
+                if (!excluded) {
+                    pathItem.readOperations().forEach(operation ->
+                        operation.addParametersItem(authHeader)
+                    );
+                }
+            });
+        };
     }
 }
