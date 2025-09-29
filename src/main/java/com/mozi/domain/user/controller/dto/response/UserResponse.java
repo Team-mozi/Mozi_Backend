ed98d82891f7b6b8c2c5d455b3ed8b7147ff03a3
dpwl0974 <dpwl0974@gmail.com>
2025-09-29 11:43:22 +0900

    refactor: 불필요한 반환 핉드 삭제

--- src/main/java/com/mozi/domain/user/controller/dto/response/UserResponse.java
+++ src/main/java/com/mozi/domain/user/controller/dto/response/UserResponse.java
@@ -11,23 +11,18 @@ public class UserResponse {
     @Schema(description = "회원 번호", example = "1")
     private Long userId;
 
-    @Schema(description = "이메일", example = "test@example.com")
-    private String email;
-
     @Schema(description = "닉네임", example = "멋쟁이모지")
     private String nickname;
 
     @Builder
-    private UserResponse(Long userId, String email, String nickname) {
+    private UserResponse(Long userId, String nickname) {
         this.userId = userId;
-        this.email = email;
         this.nickname = nickname;
     }
 
     public static UserResponse from(User user) {
         return UserResponse.builder()
                 .userId(user.getId())
-                .email(user.getEmail())
                 .nickname(user.getNickname())
                 .build();
     }
